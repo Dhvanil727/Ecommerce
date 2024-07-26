d717feaba37c5d7a6bb3b0fe42662d35cf010427
Dhvanil727 <dhvanilpatel727@gmail.com>
2024-07-26 23:19:22 +0530

    PRODUCT apis

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,8 @@
 import slugify from "slugify";
 import productModel from "../models/productModel.js";
 import fs from "fs";
-import { response } from "express";
+
+
 export const createproductcontroller = async (request, response) => {
   try {
     const { name, slug, description, price, category, quantity, shipping } =
@@ -44,7 +45,7 @@ export const createproductcontroller = async (request, response) => {
 
 export const getallproductscontroller=async(request,response)=>{
     try {
-        const products=await productModel.find({}).select("-photo").limit(12).sort({createdAt:-1});
+        const products=await productModel.find({}).populate('category').select("-photo").limit(12).sort({createdAt:-1});
         response.status(200).send({
             success:true,
             count_total:products.length,
@@ -59,4 +60,98 @@ export const getallproductscontroller=async(request,response)=>{
             error
         })
     }
+}
+
+export const getsingleproductscontroller=async(request,response)=>{
+  try {
+    const product=await productModel.findOne({slug:request.params.slug}).select("-photo").populate('category');
+    response.status(200).send({
+      success:true, 
+      message:"product fetched successfully",
+      product
+    })
+  } catch (error) {
+    console.log(error);
+    response.status(500).send({
+      success: false,
+      message: " error in get single products ",
+      error
+
+    })
+  }
+}
+export const productphotocontroller=async(request,response)=>{
+try {
+  const product=await productModel.findById(request.params.pid).select("photo");
+  if(product.photo.data){
+    response.set("Content-type", product.photo.contentType);
+    return response.status(200).send(product.photo.data);
+  }
+} catch (error) {
+  console.log(error);
+  response.status(500).send({
+    success: false,
+    message: " error in getting photo ",
+    error
+  })
+}
+}
+
+export const deleteproductcontroller=async(request,response)=>{
+  try {
+    const product =await productModel.findByIdAndDelete(request.params.pid).select("-photo");
+    response.status(500).send({
+      success:true,
+      message:"product deleted successfully",
+      
+    })
+  } catch (error) {
+    console.log(error);
+    response.status(500).send({
+      success: false,
+      message: " error in deleting product ",
+      error
+
+    })
+  }
+}
+
+export const updateproductcontroller=async(request,response)=>{
+  try {
+    const { name, slug, description, price, category, quantity, shipping } =
+      request.fields;
+    const { photo } = request.files;
+    switch (true) {
+      case !name:
+        response.status(500).send({ message: "Name is requires" });
+      case !description:
+        response.status(500).send({ message: "description is requires" });
+      case !price:
+        response.status(500).send({ message: "price is requires" });
+      case !category:
+        response.status(500).send({ message: "Category is requires" });
+      case !quantity:
+        response.status(500).send({ message: "quantity is requires" });
+      case photo && photo.size >100000:
+        response.status(500).send({ message: "photo size is too large" });
+    }
+    const products = await productModel.findByIdAndUpdate(request.params.pid,{...request.fields,slug:slugify(name)},{new:true});
+    if(photo){
+        products.photo.data=fs.readFileSync(photo.path);
+        products.photo.contentType=photo.type
+    }
+    await products.save();
+    response.status(200).send({
+        success:true,
+        message:"product updated successfully",
+        products
+    })
+  } catch (error) {
+    console.log(error);
+    response.status(500).send({
+      success: false,
+      message: " error in update Product ",
+      error,
+    });
+  }
 }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,14 @@
 import express from "express"
 import { isAdmin, requireSignIN } from "../middlewares/authMiddleware.js";
-import { createproductcontroller, getallproductscontroller } from "../controllers/productController.js";
+import { createproductcontroller, deleteproductcontroller, getallproductscontroller, getsingleproductscontroller, productphotocontroller, updateproductcontroller } from "../controllers/productController.js";
 import formidable from "express-formidable";
 const router=express.Router();
 
 router.post("/create-product",requireSignIN,isAdmin,formidable(),createproductcontroller);
+router.put("/update-product/:pid",requireSignIN,isAdmin,formidable(),updateproductcontroller);
 router.get("/get-products" ,getallproductscontroller);
-
+router.get("/get-products/:slug" ,getsingleproductscontroller);
+//get photo
+router.get("/product-photo/:pid",productphotocontroller);
+router.delete("/product/:pid",deleteproductcontroller);
 export default router;
